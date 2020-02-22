ed67c10d845e45eac12e49771f12e89f07d8d43c
Van Philip Panugan <vpp5010207@gmail.com>
2020-02-22 21:22:04 +0800

    14:08:24

--- lib/LogIn.dart
+++ lib/LogIn.dart
@@ -1,135 +1,147 @@
+import 'auth.service.dart';
+import 'main.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/material.dart';
 
 class LogIn extends StatefulWidget {
-  final _formKey = GlobalKey<FormState>();
-
   @override
-  _LogInState createState() => new _LogInState();
+  State<StatefulWidget> createState() => new _LogInState();
 }
 
 class _LogInState extends State<LogIn> {
+  String _status = 'no-action';
   final _formKey = GlobalKey<FormState>();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+
   @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
+  Widget build(BuildContext context) => new Scaffold(
       appBar: new AppBar(
-        title: new Text("Log in"),
+        title: new Text('Login'),
       ),
       body: Center(
         child: ListView(
-          children: <Widget>[
-            Form(
-              key: _formKey,
-              child: Center(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    /*Padding(
-                      padding: const EdgeInsets.only(
-                          top: 15.0, left: 25.0, right: 25.0),
-                      child: Center(
-                        child: Image(
-                            image: AssetImage("images/logo.jpg") //image location and name
-                        )
-                      ),
-                    ),*/ //remove comment for image
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          top: 15.0, left: 25.0, right: 25.0),
-                      child: Center(
-                        child: TextFormField(
-                          controller: _emailController,
-                          decoration: const InputDecoration(
-                            hintText: 'Enter your email',
-                          ),
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Please enter your email';
-                            } else
-                              return null;
-                          },
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 25.0, right: 25.0),
-                      child: Center(
-                        child: TextFormField(
-                          controller: _passwordController,
-                          decoration: const InputDecoration(
-                            hintText: 'Enter your password',
-                          ),
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Please enter your password';
-                            }
-                            return null;
-                          },
-                          obscureText: true,
-                        ),
-                      ),
-                    ),
-                    Center(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 16.0),
-                        child: RaisedButton(
-                          onPressed: () {
-                            // Validate will return true if the form is valid, or false if
-                            // the form is invalid.
-                            if (_formKey.currentState.validate()) {
-                              // Process data.
-                            }
-                          },
-                          child: Text('Log In'),
-                        ),
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Form(
+                  key: _formKey,
+                  child: Center(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
                         Padding(
-                          padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-                          child: Container(
-                            width: 180.0,
-                            child: RaisedButton(
-                              onPressed: () {
-                                // Validate will return true if the form is valid, or false if
-                                // the form is invalid.
-                                //if (_formKey.currentState.validate()) {
-                                  // Process data.
-                                //}
+                          padding: const EdgeInsets.only(
+                              left: 25.0, right: 25.0),
+                          child: Center(
+                            child: TextFormField(
+                              controller: _emailController,
+                              decoration: const InputDecoration(
+                                hintText: 'Enter your email',
+                              ),
+                              validator: (value) {
+                                if (value.isEmpty) {
+                                  return 'Please enter your email.';
+                                }else
+                                  return null;
                               },
-                              child: Text('Log In with FaceBook'),
                             ),
                           ),
                         ),
                         Padding(
-                          padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-                          child: Container(
-                            width: 180.0,
-                            child: RaisedButton(
-                              onPressed: () {
-                                // Validate will return true if the form is valid, or false if
-                                // the form is invalid.
-                                //if (_formKey.currentState.validate()) {
-                                  // Process data.
-                                //}
+                          padding:const EdgeInsets.only(left: 25.0, right: 25.0),
+                          child:Center(
+                            child: TextFormField(
+                              controller: _passwordController,
+                              decoration: const InputDecoration(
+                                hintText: 'Enter your password.',
+                              ),
+                              validator: (value) {
+                                if (value.isEmpty){
+                                  return 'Please enter your password';
+                                }
+                                return null;
                               },
-                              child: Text('Log In with Gmail'),
+                              obscureText: true,
                             ),
                           ),
                         ),
+                        Center(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical:  16.0),
+                            child: RaisedButton(
+                              onPressed: (){
+                                //setState(() => this._status = 'loading');
+
+                                appAuth.login().then((result) {
+                                  if (result) {
+                                    Navigator.of(context).pushReplacementNamed('/home');
+                                  } else {
+                                    setState(() => this._status = 'rejected');
+                                  }
+                                });
+                              },
+                              child: Text('Log In'),
+                            ),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                              child: Container(
+                                width: 180.0,
+                                child:RaisedButton(
+                                  onPressed: (){},
+                                  child:Text('Log In with Facebook'),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                                padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                                child: Container(
+                                  width: 180.0,
+                                  child: RaisedButton(
+                                    onPressed: (){},
+                                    child: Text('Log In with Gmail'),
+                                  ),
+                                )
+                            )
+                          ],
+                        )
                       ],
-                    )
-                  ],
-                ),
-              ),
-            )
-          ],
+                    ),
+                  )
+
+              )
+            ]
+        ),
+      )
+  );
+}
+
+
+
+
+/*
+    body: new Container(
+      child: new Center(
+        child: new RaisedButton(
+            child: new Text(
+                'Login for App (${this._status})'
+            ),
+            onPressed: () {
+              setState(() => this._status = 'loading');
+
+              appAuth.login().then((result) {
+                if (result) {
+                  Navigator.of(context).pushReplacementNamed('/home');
+                } else {
+                  setState(() => this._status = 'rejected');
+                }
+              });
+            }
         ),
       ),
-    );
-  }
-}
+    ),
+  );
+}*/
---
+++ lib/login.page.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/painting.dart';
+
+import 'main.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String _status = 'no-action';
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) => new Scaffold(
+    appBar: new AppBar(
+      title: new Text('Login'),
+    ),
+    body: Center(
+      child: ListView(
+        children: <Widget>[
+          Form(
+            key: _formKey,
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 25.0, right: 25.0),
+                    child: Center(
+                      child: TextFormField(
+                        controller: _emailController,
+                        decoration: const InputDecoration(
+                          hintText: 'Enter your email',
+                        ),
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Please enter your email.';
+                          }else
+                            return null;
+                          },
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding:const EdgeInsets.only(left: 25.0, right: 25.0),
+                    child:Center(
+                      child: TextFormField(
+                        controller: _passwordController,
+                        decoration: const InputDecoration(
+                          hintText: 'Enter your password.',
+                        ),
+                        validator: (value) {
+                          if (value.isEmpty){
+                            return 'Please enter your password';
+                          }
+                          return null;
+                        },
+                        obscureText: true,
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical:  16.0),
+                      child: RaisedButton(
+                        onPressed: (){
+                          setState(() => this._status = 'loading');
+
+                          appAuth.login().then((result) {
+                            if (result) {
+                              Navigator.of(context).pushReplacementNamed('/home');
+                            } else {
+                              setState(() => this._status = 'rejected');
+                            }
+                          });
+                        },
+                        child: Text('Log In'),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                        child: Container(
+                          width: 180.0,
+                          child:RaisedButton(
+                            onPressed: (){},
+                            child:Text('Log In with Facebook'),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                        child: Container(
+                          width: 180.0,
+                          child: RaisedButton(
+                            onPressed: (){},
+                            child: Text('Log In with Gmail'),
+                          ),
+                        )
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+
+          )
+        ]
+      ),
+      )
+  );
+}
+
+
+
+
+   /*
+    body: new Container(
+      child: new Center(
+        child: new RaisedButton(
+            child: new Text(
+                'Login for App (${this._status})'
+            ),
+            onPressed: () {
+              setState(() => this._status = 'loading');
+
+              appAuth.login().then((result) {
+                if (result) {
+                  Navigator.of(context).pushReplacementNamed('/home');
+                } else {
+                  setState(() => this._status = 'rejected');
+                }
+              });
+            }
+        ),
+      ),
+    ),
+  );
+}*/
