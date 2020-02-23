408f6787bcc56ee319c62bfa322d4157c6cf5191
Van Philip Panugan <vpp5010207@gmail.com>
2020-02-23 09:49:54 +0800

    11:48

--- lib/home.page.dart
+++ lib/home.page.dart
@@ -1,12 +1,17 @@
+import 'cards_section_alignment.dart';
+import 'cards_section_draggable.dart';
 import 'main.dart';
 import 'package:flutter/material.dart';
 
+
 class HomePage extends StatelessWidget {
+  bool showAlignmentCards = false;
+
   @override
   Widget build(BuildContext context) => new Scaffold(
     appBar: new AppBar(
       title: new Text('What\'s your ulam?'),
-      backgroundColor: Colors.orange,
+      backgroundColor: Colors.red,
       actions: <Widget>[
         FlatButton.icon(
           icon: Icon(Icons.person),
@@ -19,14 +24,54 @@ class HomePage extends StatelessWidget {
         )
       ],
     ),
-
-    body: new Container(
-      margin: new EdgeInsets.only(
-          top: 50.0
-      ),
-      alignment: Alignment.center,
-      child: new Column(
-      ),
+    body:
+    Column(
+      children: <Widget>[
+        showAlignmentCards
+            ? CardsSectionAlignment(context)
+            : CardsSectionDraggable(),
+        buttonsRow()
+      ],
     ),
+
+
   );
+
+  Widget buttonsRow() {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 48.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          FloatingActionButton(
+            mini: true,
+            onPressed: () {},
+            backgroundColor: Colors.white,
+            child: Icon(Icons.loop, color: Colors.yellow),
+          ),
+          Padding(padding: EdgeInsets.only(right: 8.0)),
+          FloatingActionButton(
+            onPressed: () {},
+            backgroundColor: Colors.white,
+            child: Icon(Icons.close, color: Colors.red),
+          ),
+          Padding(padding: EdgeInsets.only(right: 8.0)),
+          FloatingActionButton(
+            onPressed: () {},
+            backgroundColor: Colors.white,
+            child: Icon(Icons.favorite, color: Colors.green),
+          ),
+          Padding(padding: EdgeInsets.only(right: 8.0)),
+          FloatingActionButton(
+            mini: true,
+            onPressed: () {},
+            backgroundColor: Colors.white,
+            child: Icon(Icons.star, color: Colors.blue),
+          ),
+        ],
+      ),
+    );
+  }
 }
+
